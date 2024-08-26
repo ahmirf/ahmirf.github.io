a4ad61355e5adf22475e648aced4cae1db2a6a43
ahmirf <ahm.irf@gmail.com>
2024-08-25 20:36:23 -0400

    add pages

---
+++ t2t.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+txt2tags "/Users/iahmed3/Library/CloudStorage/GoogleDrive-ahm.irf@gmail.com/My Drive/SAFE Lab/my-website/personal-page/index.t2t"
+txt2tags "/Users/iahmed3/Library/CloudStorage/GoogleDrive-ahm.irf@gmail.com/My Drive/SAFE Lab/my-website/personal-page/cyber4n6.t2t"
+txt2tags "/Users/iahmed3/Library/CloudStorage/GoogleDrive-ahm.irf@gmail.com/My Drive/SAFE Lab/my-website/personal-page/nlp4n6.t2t"
